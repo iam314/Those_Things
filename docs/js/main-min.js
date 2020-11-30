7b0a25ddc3b938996b939f175056c10b790d45ca
ldachev <ldachev@melontech.com>
2020-11-30 20:59:28 +0200

    pathssss

---
+++ docs/js/main-min.js
@@ -0,0 +1,1 @@
+function openNav(){document.getElementById("mySidenav").style.width="100%",document.getElementById("main").style.marginLeft="100px"}function closeNav(){document.getElementById("mySidenav").style.width="0",document.getElementById("main").style.marginLeft="0"}$(document).on("ready",(function(){$(".lazy").slick({lazyLoad:"ondemand",infinite:!0})}));
