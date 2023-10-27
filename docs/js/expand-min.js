4274a3d18e2fdf56f735e16087de036d5d8c26d5
Lyudmil Dachev <workwith314@gmail.com>
2023-10-27 11:58:55 +0300

    js split into files so they work only on pages req

---
+++ docs/js/expand-min.js
@@ -0,0 +1,1 @@
+$(".readmore-btn").on("click",(function(){$(".expandable-text").removeClass("is-hidden"),$(".readmore-btn").addClass("is-hidden")})),$(".contract-btn").on("click",(function(){$(".readmore-btn").removeClass("is-hidden"),$(".expandable-text").addClass("is-hidden")}));
